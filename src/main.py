0d92cc430212001a2b58b370df68d1470a415f6c
scmac <scmac@192.168.1.159>
2018-10-31 22:17:47 +0000

    Adding calculate_distance method with passing unit test.

--- src/main.py
+++ src/main.py
@@ -4,21 +4,27 @@ Created on 31 Oct 2018
 @author: scmac
 '''
 
+import math
 
 def load_start():
     """
     Load initial values from file
     """
-    print('load_start accessed')
     input_file = 'input//problem_small.txt'
     try:
         with open(input_file) as in_file:
             feature_list = [i.rstrip('\n').split(' ') for i in in_file]
-        print (feature_list)
         return feature_list
     except IOError:
         print('Input file could not be accessed.')
-    
+
+def calculate_distance(current_feature, other_feature):
+
+    delta_x = abs(int(current_feature[1]) - int(other_feature[1]))
+    delta_y = abs(int(current_feature[2]) - int(other_feature[2]))
+
+    return math.sqrt(delta_x**2 + delta_y**2)
+
 if __name__ == '__main__':
     pass
 
--- test/unit.py
+++ test/unit.py
@@ -4,22 +4,26 @@ Created on 31 Oct 2018
 @author: scmac
 '''
 import unittest
-
+from main import calculate_distance 
 
 class TestCalculateDistance(unittest.TestCase):
-
-
+    
     def setUp(self):
         pass
-
 
     def tearDown(self):
         pass
 
     def test_calculate_distance(self):
-#         x = this_feature
-        pass
-
+        """
+        Happy path 3,4,5 triangle
+        Distance = 5
+        """
+        test_feature01 = ['test_feature01', '1', '1']
+        test_feature02 = ['test_feature02', '4', '5']
+        
+        self.assertEqual(5, calculate_distance(test_feature01, test_feature02), 'Calculated distance incorrect')
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
