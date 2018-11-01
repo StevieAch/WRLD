75a4825d5e9f251057f34c2f03f424df76076500
scmac <scmac@192.168.1.159>
2018-11-01 17:44:34 +0000

    Adding unittest 'TestFindFeaturesShortestDistance' prior to implementing 'find_features_shortest Distance'

--- src/wrld.py
+++ src/wrld.py
@@ -3,7 +3,6 @@ Created on 31 Oct 2018
 
 @author: scmac
 '''
-
 import math
 
 def load_start():
@@ -14,18 +13,26 @@ def load_start():
     try:
         with open(input_file) as in_file:
             feature_list = [i.rstrip('\n').split(' ') for i in in_file]
+        for i in feature_list:
+            print(i)
+
         return feature_list
     except IOError:
         print('Input file could not be accessed.')
 
-def calculate_distance(current_feature, other_feature):
+def find_features_shortest_distance(start_index, feature_list):
+    pass
 
+def calculate_distance(current_feature, other_feature):
+    """
+    Calculate the distance between the two 
+    parameter features via Pythagoras' Theorem
+    """
     delta_x = abs(int(current_feature[1]) - int(other_feature[1]))
     delta_y = abs(int(current_feature[2]) - int(other_feature[2]))
-
     return math.sqrt(delta_x**2 + delta_y**2)
 
 if __name__ == '__main__':
     pass
 
-features = load_start()
+load_start()
--- test/unit.py
+++ test/unit.py
@@ -4,7 +4,8 @@ Created on 31 Oct 2018
 @author: scmac
 '''
 import unittest
-from main import calculate_distance 
+from wrld import calculate_distance
+from wrld import find_features_shortest_distance
 
 class TestCalculateDistance(unittest.TestCase):
     
@@ -19,10 +20,31 @@ class TestCalculateDistance(unittest.TestCase):
         Happy path 3,4,5 triangle
         Distance = 5
         """
-        test_feature01 = ['test_feature01', '1', '1']
-        test_feature02 = ['test_feature02', '4', '5']
+        test_feature00 = ['test_feature00', '1', '1']
+        test_feature01 = ['test_feature01', '4', '5']
         
-        self.assertEqual(5, calculate_distance(test_feature01, test_feature02), 'Calculated distance incorrect')
+        output = calculate_distance(test_feature00, test_feature01)
+        print('Expected: 5, actual: ' + str(output))
+        
+        self.assertEqual(5, output, 'Calculated distance incorrect')
+        
+class TestFindFeaturesShortestDistance(unittest.TestCase):
+    
+    def test_simple_shortest_distance(self):
+        """
+    Test using four primitive Pythagorean triples to 
+    verify that the shortest distance is returned
+    """
+        test_feature00 = ['test_feature00', '1', '1'] # base coordinate
+        test_feature01 = ['test_feature01', '4', '5'] # (2, 4) distance: 5 
+        test_feature02 = ['test_feature02', '6', '13'] # (5, 12) distance: 13
+        test_feature03 = ['test_feature03', '9', '16'] # (8, 15) distance: 17
+        test_feature_list =\
+        [test_feature00, test_feature01, test_feature02, test_feature03]
+        start_index = 0
+        
+        output = find_features_shortest_distance(start_index, test_feature_list)
+        self.assertEqual(['test_feature00', 'test_feature03', 17], output, 'Features longest distance incorrect')
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
